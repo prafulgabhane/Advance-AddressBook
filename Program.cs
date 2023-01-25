d97a38338f7586dfba451315fe5768e5a8d22f80
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-25 20:20:02 +0530

    Adding files to remote UC 3

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CollectionAddressBook
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            AddressBook Adr = new AddressBook();
+            Adr.createUser();
+            Adr.editContact();
+            Adr.printUser();
+        }
+    }
+}
